e3dc4faf70ca09718a2d20d5c47b449389e8c153
Alexey Martemyanov <mallexxx@gmail.com>
2024-02-28 16:17:28 +0600

    swiftlint Plugin (#1)
    Asana Task: https://app.asana.com/0/1201037661562251/1206699853968688/f
    BSK PR: duckduckgo/BrowserServicesKit#680
    macOS PR: duckduckgo/macos-browser#2279
    iOS PR: duckduckgo/iOS#2507

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import Foundation
+
+let package = Package(
+    name: "AppleToolbox",
+    platforms: [
+        .iOS("14.0"),
+        .macOS("11.4")
+    ],
+    products: [
+        .plugin(name: "SwiftLintPlugin", targets: ["SwiftLintPlugin"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .plugin(
+            name: "SwiftLintPlugin",
+            capability: .buildTool(),
+            dependencies: [
+                .target(name: "SwiftLintBinary", condition: .when(platforms: [.macOS]))
+            ]
+        ),
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "963121d6babf2bf5fd66a21ac9297e86d855cbc9d28322790646b88dceca00f1"
+        ),
+    ]
+)
