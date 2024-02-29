b438ac3ca6c04209e93467f7b3e0aeaed6c442f1
Alexey Martemyanov <mallexxx@gmail.com>
2024-02-29 14:57:07 +0600

    move URL macro from BSK

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+import CompilerPluginSupport
 import PackageDescription
-import Foundation
 
 let package = Package(
     name: "AppleToolbox",
@@ -12,8 +12,12 @@ let package = Package(
     ],
     products: [
         .plugin(name: "SwiftLintPlugin", targets: ["SwiftLintPlugin"]),
+        .library(name: "Macros", targets: ["Macros"]),
     ],
     dependencies: [
+        // Depend on the Swift 5.9 release of SwiftSyntax
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", from: "0.2.2"),
     ],
     targets: [
         .plugin(
@@ -27,6 +31,30 @@ let package = Package(
             name: "SwiftLintBinary",
             url: "https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip",
             checksum: "963121d6babf2bf5fd66a21ac9297e86d855cbc9d28322790646b88dceca00f1"
+        ),
+        // Macro implementation that performs the source transformation of a macro.
+        .macro(
+            name: "MacrosImplementation",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+            ],
+            plugins: [.plugin(name: "SwiftLintPlugin")]
+        ),
+        // Library that exposes a macro as part of its API, which is used in client programs.
+        .target(
+            name: "Macros",
+            dependencies: ["MacrosImplementation"],
+            plugins: [.plugin(name: "SwiftLintPlugin")]
+        ),
+        .testTarget(
+            name: "MacrosTests",
+            dependencies: [
+                "MacrosImplementation",
+                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+                .product(name: "MacroTesting", package: "swift-macro-testing"),
+            ],
+            plugins: [.plugin(name: "SwiftLintPlugin")]
         ),
     ]
 )
