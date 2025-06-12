0ec56b1b3989a9ed575cf726cd672d30ace354e0
Alexey Martemyanov <mallexxx@gmail.com>
2025-06-12 21:13:43 +0600

    Update swift-syntax url

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the Swift 5.9 release of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax", "509.0.0"..<"602.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", exact: "0.2.2"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.4.0"),
     ],
