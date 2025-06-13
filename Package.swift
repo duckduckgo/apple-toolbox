ebdfb5f41a3cfe6d1570b42fbc6368da5be1f45e
Alexey Martemyanov <mallexxx@gmail.com>
2025-06-13 12:02:08 +0600

    Set swift-syntax ver to "509.0.0"..<"602.0.0" (#12)
    * set swift-syntax to "509.0.0"..<"602.0.0"
    
    * Update swift-syntax url
    
    * update swift-macro-testing

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Depend on the Swift 5.9 release of SwiftSyntax
-        .package(url: "https://github.com/apple/swift-syntax.git", exact: "509.1.1"),
-        .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", exact: "0.2.2"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", exact: "0.6.3"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.4.0"),
     ],
     targets: [
